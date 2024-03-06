1924c84438c07a2d6f6479f3a41b508a5dabf307
Qingcai Jiang <jqc@mail.ustc.edu.cn>
2024-03-06 15:42:42 +0100

    Upload a small trace that contains page-table access

--- traces_small/collect_trace.sh
+++ traces_small/collect_trace.sh
@@ -1,6 +1,8 @@
-app="/root/codes/random_access/randomAccess"
+app="/root/codes/GraphBIG/benchmark/bench_BFS/bfs"
+# app="/root/codes/random_access/randomAccess"
 instruction=
 
-../sniper/record-trace -o randomAccess -- $app
+# ../sniper/record-trace -o randomAccess -- $app
+../sniper/record-trace -d 30000000 -o GraphBIG_BFS_amazon0302_30M -- $app --dataset /mnt/panzer/rahbera/graph_datasets/graphBIG/CL-10M-1d8-L5
 
 
